628cc0537a5349c1304239ad4e9c9f14f00131bf
feskolech <feskolech@protonmail.com>
2023-02-13 16:06:06 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 set -e
-rm -rf /github/home/kek
-mkdir /github/home/kek/
-cd /github/home/kek/
+mkdir /github/home
+cd /github/home
 echo Install dependencies.
 echo deb http://deb.debian.org/debian bullseye-backports main >> /etc/apt/sources.list
 apt-get update > /dev/null 2>&1
@@ -44,11 +43,15 @@ cmake -GNinja .. > /dev/null 2>&1
 ninja -j$(nproc) > /dev/null 2>&1
 echo Fetch additional dependencies.
 cd ../..
+pwd
+ls -la
 git clone --depth 1 --recursive https://github.com/google/ngx_brotli > /dev/null 2>&1
 git clone --depth 1 https://github.com/leev/ngx_http_geoip2_module > /dev/null 2>&1
 git clone --depth 1 https://github.com/openresty/headers-more-nginx-module > /dev/null 2>&1
 echo Build nginx.
 cd ..
+pwd
+ls
 sed -i 's|CFLAGS=""|CFLAGS="-Wno-ignored-qualifiers"|g' rules
 sed -i 's|--sbin-path=/usr/sbin/nginx|--sbin-path=/usr/sbin/nginx --add-module=$(CURDIR)/debian/modules/ngx_brotli --add-module=$(CURDIR)/debian/modules/ngx_http_geoip2_module --add-module=$(CURDIR)/debian/modules/headers-more-nginx-module|g' rules
 sed -i 's|--with-cc-opt="$(CFLAGS)" --with-ld-opt="$(LDFLAGS)"|--with-cc-opt="-I../modules/boringssl/include $(CFLAGS)" --with-ld-opt="-L../modules/boringssl/build/ssl -L../modules/boringssl/build/crypto $(LDFLAGS)"|g' rules
@@ -58,8 +61,12 @@ sed -i 's|--with-http_addition_module --with-http_auth_request_module --with-htt
 sed -i 's|--with-http_stub_status_module||g' rules
 sed -i 's|--with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module|--with-http_v3_module --with-pcre-jit --without-select_module --without-poll_module --with-http_access_module --without-http_autoindex_module --without-http_browser_module --without-http_charset_module --without-http_empty_gif_module --with-http_limit_conn_module --with-http_memcached_module --without-http_mirror_module --without-http_referer_module --without-http_split_clients_module --without-http_scgi_module --without-http_ssi_module --with-http_upstream_hash_module --with-http_upstream_ip_hash_module --with-http_upstream_keepalive_module --with-http_upstream_least_conn_module --with-http_upstream_random_module --with-http_upstream_zone_module|g' rules
 cd ..
+pwd
+ls
 dpkg-buildpackage -b > /dev/null 2>&1
 cd ..
+pwd
+ls
 cp nginx_*.deb nginx.deb
 hash=$(sha256sum nginx.deb | awk '{print $1}')
 patch=$(cat /github/workspace/patch)
